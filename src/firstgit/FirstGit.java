5e2a2f91674d0b2dd08ff89da5a034a3d2f6c028
gclark7 <gclark7@L16415039539.wctc.msft>
2013-08-27 16:12:52 -0500

    Revert "2nd add modified main method sout"
    This reverts commit c1ad5d8957ff3b18e0b5359e188f97c0f084c2c6.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -15,9 +15,5 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
-        System.out.println("Yo");
-        
     }
 }
